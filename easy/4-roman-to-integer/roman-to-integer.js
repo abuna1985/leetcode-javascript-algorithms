951a52227564c85894da167a5b130a58040405fc
Adam Abundis <adamjabundis@gmail.com>
2022-04-25 13:01:06 -0700

    add comments to roman to integer js file

--- easy/4-roman-to-integer/roman-to-integer.js
+++ easy/4-roman-to-integer/roman-to-integer.js
@@ -21,28 +21,31 @@ var romanToInt = function(s) {
 	};
 	// store result of loop logic in result
 	let result = 0;
-
+  // Check if the parameter s is a string
 	if (typeof s !== 'string') {
 		throw new Error(
 			`You must provide an String for s. You provided: ${typeof s}`
 		);
 	}
-
+  // Check if all string characters are roman numerals
 	// https://www.w3docs.com/snippets/javascript/how-to-check-whether-a-string-matches-a-regex-in-javascript.html
 	if (!/^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/.test(s)) {
 		throw new Error(
 			`You must provide a String of roman numerals. You provided: ${s}`
 		);
 	}
-
+  // loop through string
 	for (i = 0; i < s.length; i++) {
+    // if current character is less than the next character
 		if (symbolMap[s[i]] < symbolMap[s[i + 1]]) {
+      // subtract the current number value and go to the next character
 			result -= symbolMap[s[i]];
 			continue;
 		}
+    // else add the character number value 
 		result += symbolMap[s[i]];
 	}
-
+  // return the number result
 	return result;
 };
 
